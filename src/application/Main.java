518ec38e1fd191b81f58b0984649839d6bb9fa83
Damian <izydor.sporna@gmail.com>
2015-03-18 14:47:28 +0100

    new content

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,8 +9,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import application.views.MenuControler;
 
 
 
@@ -27,19 +27,19 @@ public class Main extends Application {
 	
 	
 	public void start(Stage primaryStage) {
+		
 		try {
 			primaryStage.setTitle("tictactoe");
 			BorderPane root = new BorderPane();
 			Canvas canvas = new Canvas(120,120);
-			draw(canvas.getGraphicsContext2D());
-			
+		//	draw(canvas.getGraphicsContext2D());
 		//	root.setCenter(canvas);
 		//	BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,500, 300);
-			AnchorPane menuPane = createMenuPane();
-			AnchorPane HotSeatMenu = createHotSeatMenu();
-			root.setCenter(HotSeatMenu);
-		//	root.setCenter(menuPane);
+			Scene scene = new Scene(root, 600, 400);
+			BorderPane menuPane = createMenuPane();
+		//	AnchorPane HotSeatMenu = createHotSeatMenu();
+			//root.setCenter(HotSeatMenu);
+			root.setCenter(menuPane);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -48,9 +48,25 @@ public class Main extends Application {
 		}
 	}
 	
-	private AnchorPane createMenuPane() {
+	
+	
+	private BorderPane createMenuPane() {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(Main.class.getResource("views/Menu.fxml"));
+		try {
+			BorderPane menu = loader.load();
+			MenuControler controller = loader.getController();
+			controller.setMain(this);
+			return menu;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public AnchorPane createHotSeatMenu() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("views/HotSeatMenu.fxml"));
 		try {
 			AnchorPane menu = loader.load();
 			return menu;
@@ -60,9 +76,9 @@ public class Main extends Application {
 		return null;
 	}
 	
-	private AnchorPane createHotSeatMenu() {
+	public AnchorPane createComputerMenu() {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("views/HotSeatMenu.fxml"));
+		loader.setLocation(Main.class.getResource("views/ComputerMenu.fxml"));
 		try {
 			AnchorPane menu = loader.load();
 			return menu;
@@ -83,34 +99,53 @@ public class Main extends Application {
 //		
 //		g.strokeRect(50, 50, 20, 20);
 		
-		g.strokeLine(30, 60, 90, 60);		
-		
 //		g.clearRect(0, 0, 120, 120);
 	
 		
 		drawBoard(g);
-		drawCircle(g, 0, 0);
+//		drawCircle(g, 0, 0);
+//		drawCircle(g, 0, 1);
+//		drawCircle(g, 2, 0);
+//		drawCircle(g, 0, 2);
+//		drawCircle(g, 2, 2);
+		
 		drawCross(g, 1, 1);
-	
-		g.setStroke(Paint.valueOf("white"));
-	}
-	
-	
-	
-	
-
-	private void drawCross(GraphicsContext g, int i, int j) {
-		
+		drawCross(g, 2, 2);
+		drawCross(g, 2, 0);
+		drawCross(g, 0, 2);
 		
 	}
 
-	private void drawCircle(GraphicsContext g, int i, int j) {
+	
+	private void drawCross(GraphicsContext g, int row, int col) {
+		g.strokeLine(col * 40, row * 40, (col + 1) * 40, (row + 1) * 40);
+		g.strokeLine(col * 40, (row + 1) * 40, (col + 1) * 40, row * 40);
+	}
+
+	private void drawCircle(GraphicsContext g, int row, int col) {
+		g.strokeOval(40 * row, 40 * col, 40, 40);
 		
-		
+//		g.strokeOval(0, 40, 40, 40);
+//		g.strokeOval(40, 0, 40, 40);
+//		g.strokeOval(40, 40, 40, 40);
+//		g.strokeOval(0, 0, 40, 40);
+//		g.strokeOval(80, 80, 40, 40);
+//		g.strokeOval(0, 80, 40, 40);
+//		g.strokeOval(80, 0, 40, 40);
+//		g.strokeOval(40, 80, 40, 40);
+//		g.strokeOval(80, 40, 40, 40);
 	}
 
 	private void drawBoard(GraphicsContext g) {
+		g.strokeLine(0 , 0, 120, 0);	
+		g.strokeLine(0 , 120, 120, 120);
+		g.strokeLine(120 , 0, 120, 120);
+		g.strokeLine(0 , 0, 0, 120);
 		
+		g.strokeLine(0 , 40, 120, 40);	
+		g.strokeLine(0 , 80, 120, 80);
+		g.strokeLine(80 , 0, 80, 120);
+		g.strokeLine(40 , 0, 40, 120);
 	}
 
 	public static void main(String[] args) {
---
+++ src/application/views/MenuControler.java
@@ -0,0 +1,38 @@
+package application.views;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import application.Main;
+
+public class MenuControler {
+
+	@FXML
+	private Pane leftPane;
+	
+	private Main main;
+	public void setMain(Main main) {
+		this.main = main;
+	}
+	
+	
+	
+	@FXML
+	public void handleHotSeatButton() {		
+		
+		AnchorPane hotSeatPane = main.createHotSeatMenu();
+		leftPane.getChildren().clear();
+		leftPane.getChildren().add(hotSeatPane);
+	}
+	
+	@FXML
+	public void handleComputerButton() {
+		
+		
+		AnchorPane computerMenu = main.createComputerMenu();
+		leftPane.getChildren().clear();
+		leftPane.getChildren().add(computerMenu);
+	}
+	
+	
+}
